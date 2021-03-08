62badcb363838c14096c93b9dee58391b347cb62
NXPGaming <78311853+NXPGaming@users.noreply.github.com>
2021-03-08 21:13:55 +0700

    Update A7.cpp

--- A7.cpp
+++ A7.cpp
@@ -1,14 +1,25 @@
-/*
-ham xu li xau 
-strcmp()
-*/
-
 #include <iostream>
+#include <string.h>
 using namespace std;
 
 int main()
 {
-	char c = 'C';
-	double* p = &c;
+  char key[] = "apple";
+  char buffer[80]="apple apple apple apple";
+  string c=buffer[0];
+  int dem=0;
+  for(int i=1;i<buffer.length();i++)
+  {
+      if(buffer[i]==' ')
+      {
+          if(strcmp(key,c) == 0)
+          {
+              dem++;
+          }
+          string c;
+      }
+      else c += buffer[i];
+  }
+  cout<<dem;
 
 }
